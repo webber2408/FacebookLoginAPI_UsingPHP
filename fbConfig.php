ad3ae0c71788ac35ef7cdf285fe2f887ca35d4fe
webber2408 <webcrat.tech@gmail.com>
2017-09-21 14:55:13 +0530

    final commit by rahul

---
+++ fbConfig.php
@@ -0,0 +1,46 @@
+<?php
+if(!session_id()){
+    session_start();
+}
+
+// Include the autoloader provided in the SDK
+require_once __DIR__ . '/php-graph-sdk/src/Facebook/autoload.php';
+
+// Include required libraries
+use Facebook\Facebook;
+use Facebook\Exceptions\FacebookResponseException;
+use Facebook\Exceptions\FacebookSDKException;
+
+/*
+ * Configuration and setup Facebook SDK
+ */
+$appId         = '1309106659218875'; //Facebook App ID
+$appSecret     = '8e4166ddfeb5d8cb9d43710a8f51dedd'; //Facebook App Secret
+$redirectURL   = 'http://localhost/testloginapi/index.php'; //Callback URL
+$fbPermissions = array('email');  //Optional permissions
+
+$fb = new Facebook(array(
+    'app_id' => $appId,
+    'app_secret' => $appSecret,
+    'default_graph_version' => 'v2.2',
+));
+
+// Get redirect login helper
+$helper = $fb->getRedirectLoginHelper();
+
+// Try to get access token
+try {
+    if(isset($_SESSION['facebook_access_token'])){
+        $accessToken = $_SESSION['facebook_access_token'];
+    }else{
+          $accessToken = $helper->getAccessToken();
+    }
+} catch(FacebookResponseException $e) {
+     echo 'Graph returned an error: ' . $e->getMessage();
+      exit;
+} catch(FacebookSDKException $e) {
+    echo 'Facebook SDK returned an error: ' . $e->getMessage();
+      exit;
+}
+
+?>
